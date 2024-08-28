13696fefc105b47cd323dff5b3dd7250069b8975
Shivendra Singh Chauhan <150776299+shivendrasinghchauhan@users.noreply.github.com>
2024-08-28 08:03:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-/*!
 
- =========================================================
- * Paper Kit 2 - v2.0.0
- =========================================================
-
- * Product Page: http://www.creative-tim.com/product/paper-kit-2
- * Copyright 2017 Creative Tim (http://www.creative-tim.com)
- * Licensed under MIT (https://github.com/timcreative/paper-kit/blob/master/LICENSE.md)
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- */
 
  var searchVisible = 0;
  var transparent = true;
